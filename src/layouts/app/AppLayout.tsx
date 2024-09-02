8b4ecd968ad8db8f17f04121e915771fc466a55a
Jose <Jose.Diago@kyndryl.com>
2024-09-02 09:34:38 +0800

    Adding the initial header and side menu.

--- src/layouts/app/AppLayout.tsx
+++ src/layouts/app/AppLayout.tsx
@@ -1,7 +1,30 @@
 import { useEffect, useState } from "react";
+import {
+  Header,
+  HeaderContainer,
+  HeaderGlobalAction,
+  HeaderGlobalBar,
+  HeaderMenuButton,
+  HeaderName,
+  Loading,
+  SideNav,
+  SideNavItems,
+  SideNavLink,
+  SkipToContent,
+  Theme,
+} from "@carbon/react";
+import { HeaderContainerRenderProps } from "@carbon/react/lib/components/UIShell/HeaderContainer";
 import { useNavigate } from "react-router";
+import {
+  Categories,
+  Dashboard,
+  Product,
+  User,
+  UserMultiple,
+} from "@carbon/icons-react";
+import { format } from "date-fns";
 import useSessionStore from "../../stores/sessionStore";
-import { Loading } from "@carbon/react";
+import classes from "./AppLayout.module.scss";
 
 function AppLayout({ children }: { children: React.ReactNode }) {
   const { session } = useSessionStore();
@@ -18,7 +41,67 @@ function AppLayout({ children }: { children: React.ReactNode }) {
 
   if (!isRendered) return <Loading />;
 
-  return <div>{children}</div>;
+  return (
+    <HeaderContainer
+      render={({
+        isSideNavExpanded,
+        onClickSideNavExpand,
+      }: HeaderContainerRenderProps) => (
+        <>
+          <Header aria-label="Invencio, an Inventory Management System">
+            <SkipToContent />
+            <HeaderMenuButton
+              aria-label={isSideNavExpanded ? "Close menu" : "Open menu"}
+              onClick={onClickSideNavExpand}
+              isActive={isSideNavExpanded}
+              aria-expanded={isSideNavExpanded}
+            />
+            <HeaderName href="#" prefix="">
+              Invencio
+            </HeaderName>
+            <div>
+              <p className={classes.currentDate}>
+                {format(new Date(), "PPPP")}
+              </p>
+            </div>
+            <HeaderGlobalBar>
+              <HeaderGlobalAction
+                aria-label={session?.email}
+                onClick={() => {}}
+                tooltipAlignment="end"
+              >
+                <User size={20} />
+              </HeaderGlobalAction>
+            </HeaderGlobalBar>
+            <Theme theme="g90">
+              <SideNav
+                aria-label="Side navigation"
+                expanded={isSideNavExpanded}
+                onSideNavBlur={onClickSideNavExpand}
+                href="#main-content"
+              >
+                <SideNavItems>
+                  <SideNavLink renderIcon={Dashboard} href="#">
+                    Dashboard
+                  </SideNavLink>
+                  <SideNavLink renderIcon={UserMultiple} href="#">
+                    Users Management
+                  </SideNavLink>
+                  <SideNavLink renderIcon={Categories} href="#">
+                    Product Categories
+                  </SideNavLink>
+                  <SideNavLink renderIcon={Product} href="#">
+                    Products
+                  </SideNavLink>
+                </SideNavItems>
+              </SideNav>
+            </Theme>
+          </Header>
+          <div className={classes.container}>{children}</div>
+        </>
+      )}
+    />
+  );
 }
 
 export default AppLayout;
